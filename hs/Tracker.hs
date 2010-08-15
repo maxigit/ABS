149ebd3922a349c15806ed423ce8f64ccb307ffd
Maxime Bourget <mb14@sanger.ac.uk>
2010-08-15 11:07:36 +0100

    Add Track

--- hs/Tracker.hs
+++ hs/Tracker.hs
@@ -1,15 +1,18 @@
 module Tracker where
 
 
--- Command : what is sent to channel
+--Command : what is sent to channel
 data Command = Command { name :: String, args :: [String] } deriving (Show, Eq)
+type CommandSet = [Command]
 
--- we want command to be sorted by subchannel
-data CommandSet = CommandSet { subchannel :: (Maybe String), commands :: [Command] }
+--we want command to be sorted by subchannel
+--no sub channel for now data  CommandSet = CommandSet { subchannel :: (Maybe String), commands :: CommandSet }
 
--- time
+--time
 data Beat = Beat [Int] deriving (Show, Eq)
-data Time = Time { bar :: Int,  beat :: Beat}
+data Time = Time Integer deriving (Show , Eq)
 
--- Tracker line , a time _ a a list of command for each track
-data Line = Line Time [Track [CommandSet]];
+--Tracker line , a time _ a a list of command for each track
+data Line = Line Time [Track CommandSet]
+
+data Track = [Time CommandSet]
