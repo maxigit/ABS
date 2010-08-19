7e40f4c80ebc40fd260961ff8b22f95833cbccfc
Maxime Bourget <mb14@sanger.ac.uk>
2010-08-19 21:58:27 +0100

    tracker ...

--- hs/Tracker.hs
+++ hs/Tracker.hs
@@ -1,18 +1,14 @@
 module Tracker where
 
+type Channel = String
+type Beat = Rational
 
 --Command : what is sent to channel
 data Command = Command { name :: String, args :: [String] } deriving (Show, Eq)
-type CommandSet = [Command]
 
+data Event = Event Beat Command deriving (Show, Eq)
 --we want command to be sorted by subchannel
 --no sub channel for now data  CommandSet = CommandSet { subchannel :: (Maybe String), commands :: CommandSet }
 
---time
-data Beat = Beat [Int] deriving (Show, Eq)
-data Time = Time Integer deriving (Show , Eq)
 
---Tracker line , a time _ a a list of command for each track
-data Line = Line Time [Track CommandSet]
-
-data Track = [Time CommandSet]
+data Track = Track { lastBeat :: Beat, events :: [Event]}
