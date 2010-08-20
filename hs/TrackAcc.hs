a657115b3002f137f7408786f77036770f14578c
Maxime Bourget <mb14@sanger.ac.uk>
2010-08-20 08:24:40 +0100

    TrackState is a Monoid :)

---
+++ hs/TrackAcc.hs
@@ -0,0 +1,19 @@
+module TrackAcc where
+import Data.Map
+import Tracker
+import Data.Monoid
+
+{-TrackAcc is used by the Parser and store , all the intermediatary states need to parse it-}
+data TrackState = TrackState { lastBeat :: Beat, insertPoint :: Beat, events :: [Event]}
+data TrackAcc = TrackAcc (Map Channel  TrackState )
+
+-- TrackAcc
+instance Monoid TrackState where
+  mempty = TrackState 0 0 []
+  mappend (TrackState l i es) (TrackState l' i' es') = TrackState (l+l') (i+i') (es++es')
+
+--addTrack = reduce pushEventToTra
+-- make Event a monad to manage the time => Timed , we might to add the duration
+pushEvent :: TrackAcc -> Channel -> Event -> Beat -> TrackAcc
+pushEvent (TrackAcc m) ch ev d = TrackAcc $ insertWith mappend ch v m where
+  v = TrackState d d [ev]
