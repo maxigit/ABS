12256376d6bd9d73046e0e0fbe568f7d4240fb9a
Maxime Bourget <mb14@sanger.ac.uk>
2010-08-20 23:18:52 +0100

    Event back as a type synomy

--- hs/Tracker.hs
+++ hs/Tracker.hs
@@ -9,10 +9,13 @@ data Command = Command { name :: String, args :: [String] } deriving (Show, Eq)
 
 {-data Event = Event { beat :: Beat, command ::  Command } deriving (Show, Eq)-}
 type RationalM = Sum Rational
-newtype Event =  Event (Writer RationalM Command)
+type Event =  Writer RationalM Command
 
-instance Show Event  where
-  show (Event ev) = (show d) ++ " : " ++ (show c) where (c, d) = runWriter ev
+{-instance Show Event  where-}
+  {-show (Event ev) = (show d) ++ " : " ++ (show c) where (c, d) = runWriter ev-}
+
+instance (Show w, Show a) => Show (Writer w a) where
+  show (Writer (a, w)) =  (show w) ++ " : " ++ (show a)
 --we want command to be sorted by subchannel
 --no sub channel for now data  CommandSet = CommandSet { subchannel :: (Maybe String), commands :: CommandSet }
 
